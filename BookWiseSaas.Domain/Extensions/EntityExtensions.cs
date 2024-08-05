094154bec0097ef986a44fb6664c1b9f854072e6
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-05 16:28:00 +0300

    Extensions Added

---
+++ BookWiseSaas.Domain/Extensions/EntityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BookWiseSaas.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWiseSaas.Domain.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static bool IsValid(this IEntity entity)
+        {
+            return entity != null && entity.Id != Guid.Empty;
+        }
+    }
+}
