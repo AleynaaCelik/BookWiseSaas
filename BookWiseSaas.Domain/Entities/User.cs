d679a4d50431ad3f5cfb7e4f8fdd3de2ce450c8c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-07 23:46:57 +0300

    User  Updated

---
+++ BookWiseSaas.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWiseSaas.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; } // Şifre hash'i
+    }
+}
