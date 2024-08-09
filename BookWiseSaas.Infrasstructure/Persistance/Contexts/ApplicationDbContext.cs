e343889e25ed126181e5a56ce4a741f843e6e51d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-09 23:54:16 +0300

    ApplicationDbContextFactory Added

---
+++ BookWiseSaas.Infrasstructure/Persistance/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer4.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWiseSaas.Infrastructure.Persistance.Contexts
+{
+    public class ApplicationDbContext 
+    {
+    }
+}
---
+++ BookWiseSaas.Infrasstructure/Persistance/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWiseSaas.Infrastructure.Persistance.Contexts
+{
+    internal class ApplicationDbContextFactory
+    {
+    }
+}
