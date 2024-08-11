8ac9f5c7a68bf1e71e5877a58344879746a9cf59
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-11 22:53:40 +0300

    UserLogin Configuration Added

---
+++ BookWiseSaas.Infrasstructure/Persistance/Configuration/UserLoginConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWiseSaas.Infrastructure.Persistance.Configuration
+{
+    public class UserLoginConfiguration : IEntityTypeConfiguration<>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<UserLogin> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
