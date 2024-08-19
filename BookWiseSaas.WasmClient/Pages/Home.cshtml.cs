646d9b7517ad0d518d55748b9b9cc5af044b3334
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-19 23:23:43 +0300

    HomeRazor Added

---
+++ BookWiseSaas.WasmClient/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookWiseSaas.WasmClient.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
