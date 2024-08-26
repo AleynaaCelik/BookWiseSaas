d576a1d969bdd7d2532a3c85e54941180b23c014
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-26 23:30:17 +0300

    [Feature]: SignalR implementation started.

--- BookWiseSaas.WebApi/Program.cs
+++ BookWiseSaas.WebApi/Program.cs
@@ -1,25 +1,85 @@
-var builder = WebApplication.CreateBuilder(args);
+using BookWiseSaas.Application;
+using BookWiseSaas.Application.Common.Interfaces;
+using BookWiseSaas.Domain.Settings;
+using BookWiseSaas.Infrastructure;
+using BookWiseSaas.WebApi;
+using BookWiseSaas.WebApi.Filters;
+using BookWiseSaas.WebApi.Hubs;
+using BookWiseSaas.WebApi.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    // .WriteTo.File("C:\\Users\\alper\\Desktop\\log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("/Users/altudev/Desktop/logs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog();
+
+    // Add services to the container.
+    builder.Services.AddControllers(opt =>
+    {
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
+
+    builder.Services.AddApplication();
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddWebServices(builder.Configuration);
+
+    builder.Services.Configure<ApiBehaviorOptions>(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+
+    builder.Services.AddSingleton<IRootPathService>(new RootPathManager(builder.Environment.WebRootPath));
+
+    var app = builder.Build();
+
+    app.UseCors("AllowAll");
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseStaticFiles();
+
+    var requestLocalizationOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+
+    app.UseRequestLocalization(requestLocalizationOptions.Value);
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapHub<OrderHub>("/hubs/orderHub");
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
